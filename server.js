6e47456681ef055d146c98b4cd3c618245c74c9f
AnghelaRodriguezBarrios <anghela.rodriguez@vallegrande.edu.pe>
2022-12-13 05:57:59 -0500

    subiendo 4.0

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname+'/'));
 
 //enrutamiento
 app.get('/', (req, res) => {
-    res.sendFile("/home/ubuntu/AS221S2/Institucion.html")
+    res.sendFile("Institucion.html")
 });
 
 
