25691eb0afd4d9742e4e11a7b0ba472a5cbc119a
AnghelaRodriguezBarrios <anghela.rodriguez@vallegrande.edu.pe>
2022-12-13 05:15:10 -0500

    subiendo

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
-const app = express();
+//const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Express con Html</h1>'))
+//app.get('/', (req, res) => res.send('<h1>Express con Html</h1>'))
+app.get('/', (req, res) => {
+res.sendFile("/home/ubuntu/AS221S2/Instucion.html")
+});
+
+//Recursos
+app.use(express.static(__dirname+'/'));
 
 app.listen(3000);
 console.log('Server on port 3000')
